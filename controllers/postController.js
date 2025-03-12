a017671bb1d83a4e717fd2eb1be614cc94e7c815
RuslanGon <ruslan441006@gmail.com>
2025-03-12 13:41:57 +0200

    wer

--- controllers/postController.js
+++ controllers/postController.js
@@ -101,13 +101,19 @@ export const updatePost = async(req, res) => {
 
 export const getLastTads = async(req, res) => {
   try {
-    const posts = await Post.find().limit(5).exec()
-    const tags = posts.map(obj => obj.tags).flat().slice(0,5)
-    res.json(posts)
-  } catch (error) {
+    const posts = await Post.find().limit(5).exec();
+
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(tags);
+  } catch (err) {
     console.log(err);
     res.status(500).json({
-      message: 'Не удалось получить статьи',
+      message: 'Не удалось получить тэги',
     });
   }
+
 }
